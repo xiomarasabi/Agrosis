c1dd40333b568b33b79988a04941f964e41b5003
DilsonChimborazo <dilsonchimborazoperez@gmail.com>
2024-11-17 16:48:51 -0500

    acabando los models

--- apps/iot/models.py
+++ apps/iot/models.py
@@ -1,3 +1,22 @@
 from django.db import models
-
+from apps.trazabilidad.models import Eras
 # Create your models here.
+class Sensores(models.Model):
+    nombre_sensor = models.CharField(max_length=50)
+    tipo_sensor = models.CharField(max_length=50)
+    unidad_medida = models.CharField(max_length=50)
+    descripcion = models.TextField()
+    medida_minima = models.IntegerField()
+    medida_maxima = models.IntegerField()
+
+    def __str__(self):
+        return self.nombre_sensor
+    
+
+class Mide(models.model):
+    fk_id_sensor = models.ForeignKey(Sensores, on_delete=models.SET_NULL, null=True)
+    fk_id_era = models.ForeignKey(Eras, on_delete=models)
+
+    def __str__(self):
+        return f"Nombre del sensor: {self.fk_id_sensor.nombre_sensor} Era: {self.fk_id_era}"  
+
