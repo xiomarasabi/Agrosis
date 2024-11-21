32be12b51b2cd98b13f4b3ed6a5f8eda17333933
xiomara <xiomarasabi2003@gmail.com>
2024-11-20 19:01:26 -0500

    Se terminaron de realizar los serializers en su totalidad, de la app Trazabilidad

--- apps/trazabilidad/api/serializers.py
+++ apps/trazabilidad/api/serializers.py
@@ -1,52 +1,103 @@
-from rest_framework import serializers
-from apps.trazabilidad.models import Actividad, Realiza, Especie, TipoCultivo, Semillero, AsignacionActividades, Programacion, Notificacion, ControlUsoInsumo,CalendarioLunar
+from rest_framework.serializers import ModelSerializer
+from apps.trazabilidad.models import Actividad, Realiza, Especie, TipoCultivo, Semillero, AsignacionActividades, Programacion, Notificacion, ControlUsoInsumo,CalendarioLunar,Ubicacion,Lote,Eras,Plantacion,Desarrollan,Pea,Desarrollan,Tipo_residuos,Cultivo,Residuos,Control_fitosanitario
 
-class ActividadSerializer(serializers.ModelSerializer):
+class ActividadSerializer(ModelSerializer):
     class Meta:
         model = Actividad
         fields = '__all__'
 
-class RealizaSerializer(serializers.ModelSerializer):
+class RealizaSerializer(ModelSerializer):
     class Meta:
         model = Realiza
         fields = '__all__'
 
-class EspecieSerializer(serializers.ModelSerializer):
+class EspecieSerializer(ModelSerializer):
     class Meta:
         model = Especie
         fields = '__all__'
 
-class TipoCultivoSerializer(serializers.ModelSerializer):
+class TipoCultivoSerializer(ModelSerializer):
     class Meta:
         model = TipoCultivo
         fields = '__all__'
 
-class SemilleroSerializer(serializers.ModelSerializer):
+class SemilleroSerializer(ModelSerializer):
     class Meta:
         model = Semillero
         fields = '__all__'
 
-class AsignacionActividadSerializer(serializers.ModelSerializer):
+class AsignacionActividadSerializer(ModelSerializer):
     class Meta:
         model = AsignacionActividades
         fields = '__all__'
 
-class ProgramacionSerializer(serializers.ModelSerializer):
+class ProgramacionSerializer(ModelSerializer):
     class Meta:
         model = Programacion
         fields = '__all__'
 
-class NotificacionSerializer(serializers.ModelSerializer):
+class NotificacionSerializer(ModelSerializer):
     class Meta:
         model = Notificacion
         fields = '__all__'
 
-class ControlUsoInsumoSerializer(serializers.ModelSerializer):
+class ControlUsoInsumoSerializer(ModelSerializer):
     class Meta:
         model = ControlUsoInsumo
         fields = '__all__'
 
-class CalendarioLunarSerializer(serializers.ModelSerializer):
+class CalendarioLunarSerializer(ModelSerializer):
     class Meta:
         model = CalendarioLunar
         fields = '__all__'
+        
+        
+class UbicacionSerializer(ModelSerializer):
+    class Meta:
+        model = Ubicacion
+        fields = '__all__'
+        
+class LoteSerializer(ModelSerializer):
+    class Meta:
+        model = Lote
+        fields = '__all__'
+        
+class ErasSerializer(ModelSerializer):
+    class Meta:
+        model = Eras
+        fields = '__all__'
+        
+class CultivoSerializer(ModelSerializer):
+    class Meta:
+        model = Cultivo
+        fields = '__all__'
+class PlantacionSerializer(ModelSerializer):
+    class Meta:
+        model = Plantacion
+        fields = '__all__'
+        
+class PeaSerializer(ModelSerializer):
+    class Meta:
+        model = Pea
+        fields = '__all__'
+        
+class DesarrollanSerializer(ModelSerializer):
+    class Meta:
+        model = Desarrollan
+        fields = '__all__'
+        
+class Tipo_residuosSerializer(ModelSerializer):
+    class Meta:
+        model = Tipo_residuos
+        fields = '__all__'
+        
+class ResiduosSerializer(ModelSerializer):
+    class Meta:
+        model = Residuos
+        fields = '__all__'
+        
+class Control_fitosanitarioSerializer(ModelSerializer):
+    class Meta:
+        model = Control_fitosanitario
+        fields = '__all__'
+        
